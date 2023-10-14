2b3f0d3338946d3396cc6a1d4b09fa11c1412ac5
kuyilisolutions <146425420+kuyilisolutions@users.noreply.github.com>
2023-10-14 22:41:45 +0530

    Create serverstart.go

---
+++ pkg/Tutorial/02-HostServer/serverstart.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+    "fmt"
+    "net/http"
+)
+
+func main() {
+    http.HandleFunc("/web", processHandler)
+    http.Handle("/", http.FileServer(http.Dir("./static")))
+    
+    fmt.Println("Server is running on :8080")
+    http.ListenAndServe(":8080", nil)
+}
+
+func processHandler(w http.ResponseWriter, r *http.Request) {
+    if r.Method == "POST" {
+        r.ParseForm()
+        data := r.FormValue("data")
+        // Do something with the data, e.g., print it
+        fmt.Println("Received data:", data)
+    }
+}
